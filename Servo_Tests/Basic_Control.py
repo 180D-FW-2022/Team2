9bb788db27ab344132f122e361deffe08b970a83
GabrielGoch <gabrielgoch@ucla.edu>
2022-10-27 20:16:07 -0700

    .py

---
+++ Servo_Tests/Basic_Control.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BOARD)
+
+    # PWM Pin
+    pin = 12;
+    # Frequency
+    freq = 0.7
+    # Duty Cycle
+    duty = 50
+
+    # PWM: 12, 13
+    # Regular: 22,27,23,24,25,5,6,26
+
+    GPIO.setup(pin, GPIO.OUT)
+
+    pwm = GPIO.PWM(pin, freq)
+
+    pwm.start(duty)
+
+    sleep(150)
+
+    pwm.stop()
+
+    GPIO.cleanup()
