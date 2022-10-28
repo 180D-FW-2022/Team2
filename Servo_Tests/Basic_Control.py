c1060b13820152124020bc700e4f851796f548cb
GabrielGoch <gabrielgoch@ucla.edu>
2022-10-27 21:27:19 -0700

    Up

--- Servo_Tests/Basic_Control.py
+++ Servo_Tests/Basic_Control.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     pwm.start(duty)
 
-    for duty in range (40,80)
+    for duty in range (40,80):
         pwm.ChangeDutyCycle(duty)
         print(duty)
         time.sleep(5)
