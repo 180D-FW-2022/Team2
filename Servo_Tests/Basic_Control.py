d0707155edd70453c9d2dc9537e8ed8e66507a2c
GabrielGoch <gabrielgoch@ucla.edu>
2022-11-03 19:49:33 -0700

    Add 2nd Servo

--- Servo_Tests/Basic_Control.py
+++ Servo_Tests/Basic_Control.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
 
     # PWM Pin
-    pin = 32
+    pin1 = 32
+    pin2 = 7
     # Frequency
     freq = 400
     # Duty Cycle
@@ -14,21 +15,26 @@ if __name__ == '__main__':
 
     GPIO.setwarnings(False)
 
-    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setup(pin1, GPIO.OUT)
+    GPIO.setup(pin2, GPIO.OUT)
 
-    pwm = GPIO.PWM(pin, freq)
+    pwm1 = GPIO.PWM(pin1, freq)
+    pwm2 = GPIO.PWM(pin2, freq)
 
-    pwm.start(duty)
+    pwm1.start(duty)
+    pwm2.start(duty)
 
     #dutySpread = [54, 57.0, 57.7]
 
     dutySpread = np.arange(40, 80, 1)
 
     for duty in dutySpread:
-        pwm.ChangeDutyCycle(duty)
+        pwm1.ChangeDutyCycle(duty)
+        pwm2.ChangeDutyCycle(duty)
         print(duty)
         time.sleep(10)
 
-    pwm.stop()
+    pwm1.stop()
+    pwm2.stop()
 
     GPIO.cleanup()
