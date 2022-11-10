26796225379bbc99b7e5289c374777d10d017c9e
jimiclic <69395593+jimiclic@users.noreply.github.com>
2022-11-09 16:54:42 -0800

    Comments for Bluetooth Test

--- Bluetooth Basic Connectivity Test/bluetoothTestSender.py
+++ Bluetooth Basic Connectivity Test/bluetoothTestSender.py
@@ -1,14 +1,16 @@
 import bluetooth
 import time
 
+# Replace with bluetooth MAC address of your raspberry pi
 bd_addr = "B8:27:EB:0E:7D:93"
 
+# Bluetooth setup
 port = 1
-
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((bd_addr, port))
 
 while(1):
+    # This will just send the message below to test bluetooth connectivity
     sock.send("Hello from Surface Laptop!")
     time.sleep(2)
 
