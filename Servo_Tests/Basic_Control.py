db8197d9006e39bbaa061495f79ba461453b34e7
GabrielGoch <gabrielgoch@ucla.edu>
2022-11-20 14:47:48 -0800

    CalibrationTime

--- Servo_Tests/Basic_Control.py
+++ Servo_Tests/Basic_Control.py
@@ -35,9 +35,9 @@ if __name__ == '__main__':
 
     #dutySpread = [54, 57.0, 57.7]
 
-    dutySpread = np.arange(40, 52, 1)
+    dutySpread = np.arange(60, 80, 1)
 
-    duty1 = 65
+    duty1 = 35
     duty2 = 35
     #69.80000000000003
 
