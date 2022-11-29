14ca0117b07b5fd6f68954cbfbad24da16c251f1
Brian <brianhvu96@gmail.com>
2022-11-29 01:04:01 +0000

    Fixed motor cw/ccw issue

--- Bluetooth/Bluetooth Motor Control/bluetoothMotorPiSide.py
+++ Bluetooth/Bluetooth Motor Control/bluetoothMotorPiSide.py
@@ -15,36 +15,43 @@ GPIO.setmode(GPIO.BOARD)
 
 pin_left = 32
 pin_right = 33
-freq = 400
-
-cw_left = 49
-# tried 65
-ccw_left = 68
-cw_right = 48
-ccw_right = 61
-stop = 0
+#trieed 400, 300
+freq_cw = 200
+#tried 500
+freq_ccw = 600
+# tried 
+duty_left = 50
+# tried 
+duty_right = 50
 
 GPIO.setwarnings(False)
 GPIO.setup(pin_left, GPIO.OUT)
 GPIO.setup(pin_right, GPIO.OUT)
-pwm_left = GPIO.PWM(pin_left, freq)
-pwm_right = GPIO.PWM(pin_right, freq)
+pwm_left = GPIO.PWM(pin_left, freq_cw)
+
+pwm_right = GPIO.PWM(pin_right, freq_cw)
 
 while(1):
     data = client_sock.recv(1024)
-    print("Received: " + str(data))
+    #print("Received: " + str(data))
 
     if str(data) == "b'q'":
-        print("Moving left motor forward.")
-        pwm_left.start(cw_left)
+        #print("Moving left motor forward.")
+        pwm_left.ChangeFrequency(freq_ccw)
+        pwm_left.start(duty_left)
     elif str(data) == "b'a'":
-        print("Moving left motor backwards.")
-        pwm_left.start(ccw_left)
+        #print("Moving left motor backwards.")
+        pwm_left.ChangeFrequency(freq_cw)
+        pwm_left.start(duty_left)
     # For the right motor
     elif str(data) == "b'o'":
-        pwm_right.start(cw_right)
+        #print("Moving right motor forward.")
+        pwm_right.ChangeFrequency(freq_cw)
+        pwm_right.start(duty_right)
     elif str(data) == "b'l'":
-        pwm_right.start(ccw_right)
+        #print("Moving right motor backwards.")
+        pwm_right.ChangeFrequency(freq_ccw)
+        pwm_right.start(duty_right)
 
     time.sleep(0.5)
 
