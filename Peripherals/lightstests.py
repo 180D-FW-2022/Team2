b8a11a61f038c9e19fbd0b4cd3a2bc215dce839d
nicolasschmidt2001 <114626524+nicolasschmidt2001@users.noreply.github.com>
2023-01-18 11:35:27 -0800

    .

---
+++ Peripherals/lightstests.py
@@ -0,0 +1,58 @@
+#libraries
+import RPi.GPIO as GPIO
+import os
+from time import sleep
+#disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO Mode
+GPIO.setmode(GPIO.BCM)
+#set red,green and blue pins
+redPin = [14,27,10,25,0]
+greenPin = [15,22,9,8,5]
+bluePin = [23,24,11,7,6]
+#set pins as outputs
+for i in range(5):
+    GPIO.setup(redPin[i],GPIO.OUT)
+    GPIO.setup(greenPin[i],GPIO.OUT)
+    GPIO.setup(bluePin[i],GPIO.OUT)
+
+def turnOff():
+    for i in range(5):
+        GPIO.output(redPin[i],GPIO.LOW)
+        GPIO.output(greenPin[i],GPIO.LOW)
+        GPIO.output(bluePin[i],GPIO.LOW)
+    
+def red(i):
+    GPIO.output(redPin[i],GPIO.HIGH)
+    GPIO.output(greenPin[i],GPIO.LOW)
+    GPIO.output(bluePin[i],GPIO.LOW)
+
+def redall():
+    for i in range(5)
+        GPIO.output(redPin[i],GPIO.HIGH)
+        GPIO.output(greenPin[i],GPIO.LOW)
+        GPIO.output(bluePin[i],GPIO.LOW)
+
+def green(i):
+    GPIO.output(redPin[i],GPIO.LOW)
+    GPIO.output(greenPin[i],GPIO.HIGH)
+    GPIO.output(bluePin[i],GPIO.LOW)
+    
+def yellow(i):
+    GPIO.output(redPin[i],GPIO.HIGH)
+    GPIO.output(greenPin[i],GPIO.HIGH)
+    GPIO.output(bluePin[i],GPIO.LOW)
+    
+def fred():
+    redall()
+    sleep(.5)
+    turnOff()
+    sleep(.5)
+    redall()
+    sleep(.5)
+    turnOff()
+    sleep(.5)
+    redall()
+    sleep(.5)
+    turnOff()
+    
