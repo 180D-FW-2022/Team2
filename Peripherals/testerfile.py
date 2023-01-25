cc06f6e1c7673d52b08165eed8a51f8ba055e8a8
nicolasschmidt2001 <nicolasschmidt@ucla.edugit config --global user.email nicolasschmidt@ucla.edu>
2023-01-25 21:30:04 +0000

    j

--- Peripherals/testerfile.py
+++ Peripherals/testerfile.py
@@ -6,9 +6,12 @@ import threading
 def shot():
     call(['aplay','shot.wav'])
 playshot=threading.Thread(target=shot)
+shot2=threading.Thread(target=shot)
 startUp()
 sleep(2)
 playshot.start()
+sleep(0.5)
+shot2.start()
 playshot=threading.Thread(target=shot)
 turnOff()
 sleep(5)
