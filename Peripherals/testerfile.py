d732246aa2af6aed7ff3f56d9478d99c0a5eec2c
nicolasschmidt2001 <nicolasschmidt@ucla.edugit config --global user.email nicolasschmidt@ucla.edu>
2023-01-26 00:33:06 +0000

    y

--- Peripherals/testerfile.py
+++ Peripherals/testerfile.py
@@ -1,26 +1,10 @@
 from lightfunctions import turnOff, startUp, setHealth
 from soundfunctions import shoot
 from time import sleep
-<<<<<<< HEAD
-from subprocess import call
-import threading
-def shot():
-    call(['aplay','shot.wav'])
-playshot=threading.Thread(target=shot)
-shot2=threading.Thread(target=shot)
-startUp()
-sleep(2)
-playshot.start()
-sleep(0.5)
-shot2.start()
-playshot=threading.Thread(target=shot)
-=======
-
-
+turnOff()
 startUp()
 sleep(2)
 shoot()
->>>>>>> ee6288cbae7af3b91e3c239008e6ce7024615acb
 turnOff()
 sleep(5)
 j=100
