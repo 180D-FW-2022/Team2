b78eaa5b1af7f950c3e8518d8d81f7319e1ea0a2
jimiclic <69395593+jimiclic@users.noreply.github.com>
2023-02-01 17:31:09 -0800

    Update Pi.py

--- Bluetooth/Final Project Version/Pi.py
+++ Bluetooth/Final Project Version/Pi.py
@@ -19,9 +19,9 @@ pin_left = 32
 pin_right = 33
 
 # tried 200
-freq_cw = 100
+freq_cw = 400
 # tried 600
-freq_ccw = 600
+freq_ccw = 400
 
 duty_left = 50
 duty_right = 50
@@ -31,6 +31,9 @@ GPIO.setup(pin_left, GPIO.OUT)
 GPIO.setup(pin_right, GPIO.OUT)
 pwm_left = GPIO.PWM(pin_left, freq_cw)
 pwm_right = GPIO.PWM(pin_right, freq_cw)
+
+pwm_left.start(0)
+pwm_right.start(0)
 
 # Sound and threading for sound
 def shotsound():
@@ -52,29 +55,35 @@ while(1):
     # For the left motor
     if str(data).find("q") != -1:
         #print("Moving left motor forward.")
-        pwm_left.ChangeFrequency(freq_ccw)
-        pwm_left.start(duty_left)
+        #pwm_left.ChangeFrequency(freq_ccw)
+        pwm_left.ChangeDutyCycle(65)
+        #pwm_left.start(duty_left)
     elif str(data).find("a") != -1:
         #print("Moving left motor backwards.")
-        pwm_left.ChangeFrequency(freq_cw)
-        pwm_left.start(duty_left)
+        #pwm_left.ChangeFrequency(freq_cw)
+        pwm_left.ChangeDutyCycle(45)
+        #pwm_left.start(duty_left)
 
 
     # For the right motor
     if str(data).find("o") != -1:
         #print("Moving right motor forward.")
-        pwm_right.ChangeFrequency(freq_cw)
-        pwm_right.start(duty_right)
+        #pwm_right.ChangeFrequency(freq_cw)
+        pwm_right.ChangeDutyCycle(45)
+        #pwm_right.start(duty_right)
     elif str(data).find("l") != -1:
         #print("Moving right motor backwards.")
-        pwm_right.ChangeFrequency(freq_ccw)
-        pwm_right.start(duty_right)
+        #pwm_right.ChangeFrequency(freq_ccw)
+        pwm_right.ChangeDutyCycle(65)
+        #pwm_right.start(duty_right)
     
     time.sleep(0.1)
 
-    pwm_left.stop()
-    pwm_right.stop()
+    pwm_left.ChangeDutyCycle(0)
+    pwm_right.ChangeDutyCycle(0)
 
+pwm_left.stop()
+pwm_right.stop()
 GPIO.cleanup()
 client_sock.close()
 server_sock.close()
