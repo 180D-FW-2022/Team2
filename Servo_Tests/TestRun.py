63e5333b4aa4da470a53061b0507e2bd07341b57
jimiclic <69395593+jimiclic@users.noreply.github.com>
2023-02-01 18:39:42 -0800

    Update TestRun.py

--- Servo_Tests/TestRun.py
+++ Servo_Tests/TestRun.py
@@ -32,7 +32,6 @@ if __name__ == '__main__':
     pwm1.start(0)
     pwm2.start(0)
     print("Hold on to your butts!")
-    time.sleep(5)
 
     pwm1.ChangeDutyCycle(35)
     pwm2.ChangeDutyCycle(69)
