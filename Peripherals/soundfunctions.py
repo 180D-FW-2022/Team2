156f6b25343eecb6c124c3a5a413714e71ed9c49
nicolasschmidt2001 <114626524+nicolasschmidt2001@users.noreply.github.com>
2023-02-01 16:57:48 -0800

    g

--- Peripherals/soundfunctions.py
+++ Peripherals/soundfunctions.py
@@ -2,8 +2,16 @@ from subprocess import call
 import threading
 
 def shotsound():
-    call(['aplay','shot.wav'])
+    call(['aplay','shoot.wav'])
+
+def reloadsound():
+    call(['aplay','reload.wav'])
+
 
 def shoot():
     playshot=threading.Thread(target=shotsound)
     playshot.start()
+
+def reload():
+    playreload=threading.Thread(target=reloadsound)
+    playreload.start()
