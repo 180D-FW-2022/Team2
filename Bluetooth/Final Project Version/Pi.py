1ac6957900082ab2bc687baa3fe71e033db5e1d0
jimiclic <69395593+jimiclic@users.noreply.github.com>
2023-02-08 12:43:07 -0800

    Update Pi.py

--- Bluetooth/Final Project Version/Pi.py
+++ Bluetooth/Final Project Version/Pi.py
@@ -22,19 +22,16 @@ GPIO.setmode(GPIO.BOARD)
 pin_left = 32
 pin_right = 33
 
-# tried 200
-freq_cw = 400
-# tried 600
-freq_ccw = 800
+freq = 400
 
-duty_left = 50
-duty_right = 50
+duty_cw = 45
+duty_ccw = 65
 
 GPIO.setwarnings(False)
 GPIO.setup(pin_left, GPIO.OUT)
 GPIO.setup(pin_right, GPIO.OUT)
-pwm_left = GPIO.PWM(pin_left, freq_cw)
-pwm_right = GPIO.PWM(pin_right, freq_cw)
+pwm_left = GPIO.PWM(pin_left, freq)
+pwm_right = GPIO.PWM(pin_right, freq)
 
 #startUp()
 #setHealth(health)
@@ -57,26 +54,26 @@ while(1):
     # For the left motor
     if str(data).find("q") != -1:
         #print("Moving left motor forward.")
-        pwm_left.ChangeFrequency(freq_ccw)
-        pwm_left.start(duty_left)
+        pwm_left.ChangeFrequency(freq)
+        pwm_left.start(duty_cw)
         #pwm_left.ChangeDutyCycle(65)
     elif str(data).find("a") != -1:
         #print("Moving left motor backwards.")
-        pwm_left.ChangeFrequency(freq_cw)
-        pwm_left.start(duty_left)
+        pwm_left.ChangeFrequency(freq)
+        pwm_left.start(duty_ccw)
         #pwm_left.ChangeDutyCycle(45)
 
 
     # For the right motor
     if str(data).find("o") != -1:
         #print("Moving right motor forward.")
-        pwm_right.ChangeFrequency(freq_cw)
-        pwm_right.start(duty_right)
+        pwm_right.ChangeFrequency(freq)
+        pwm_right.start(duty_ccw)
         #pwm_right.ChangeDutyCycle(45)
     elif str(data).find("l") != -1:
         #print("Moving right motor backwards.")
-        pwm_right.ChangeFrequency(freq_ccw)
-        pwm_right.start(duty_right)
+        pwm_right.ChangeFrequency(freq)
+        pwm_right.start(duty_cw)
         #pwm_right.ChangeDutyCycle(65)
     
     time.sleep(0.1)
